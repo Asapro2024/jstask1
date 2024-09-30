70393c034823114999a7a9acb61311c96b26e1a1
Asapro2024 <computerayo123@gmail.com>
2024-09-30 16:25:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("red-btn").addEventListener("click", function() {
+    document.getElementById("rect1").style.backgroundColor = "red";
+  });
+  
+  document.getElementById("blue-btn").addEventListener("click", function() {
+    document.getElementById("rect1").style.backgroundColor = "blue";
+  });
+
+  document.getElementById("red-btn").addEventListener("click", function() {
+    document.getElementById("rect2").style.backgroundColor = "rgb(140, 3, 3)";
+  });
+  
+  document.getElementById("blue-btn").addEventListener("click", function() {
+    document.getElementById("rect2").style.backgroundColor = "rgb(12, 4, 124)";
+  });
+  
+  
